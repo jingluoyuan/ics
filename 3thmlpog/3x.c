b0dc36d7ab015ca340e8423eaa651c4cd4c9e1ac
mozhe <2399769798@qq.com>
2022-07-20 15:47:34 +0800

    hello

--- 3thmlpog/3x.c
+++
@@ -1,9 +0,0 @@
-/* 3.4 
-    unsignde char --> long
-    movzbl  (%rdi),%eax
-    movl    %eax,(%rsi)
-
-为什么是movzbl不是movzbq,作者在课程网站的勘误表errata里说了，因为movzbl和movzbq的效果完全一样，
-gcc编译器会生成movzbl。原因就是movzbl把8位零扩展成32位，同时触发x86-64的惯例
-（任何为寄存器生成32位值的指令都会把高32位置零），
-所以movzbl指令会继续把高32位置0，结果就是除了低8位放的是数值，其他56位都是0，和movzbq的效果完全一样。*/
